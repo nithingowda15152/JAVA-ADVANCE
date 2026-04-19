6b4442acb62d01b9d079af9a5de4428015485337
Nithin KV <gowdanithin232@gmail.com>
2026-04-19 23:35:03 +0530

    Add files via upload

---
+++ Spring/SpringMVC/springmvc-first/Customer.java
@@ -0,0 +1,26 @@
+package com.first;
+
+public class Customer {
+	//generate data members with same name given in the form
+	String username;
+	int userage;
+	long userphono;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getUserage() {
+		return userage;
+	}
+	public void setUserage(int userage) {
+		this.userage = userage;
+	}
+	public long getUserphono() {
+		return userphono;
+	}
+	public void setUserphono(long userphono) {
+		this.userphono = userphono;
+	}
+}
