ff6b610cfc260e4bb8f64faa3800673465656284
Nithin KV <gowdanithin232@gmail.com>
2026-04-28 22:27:58 +0530

    Add files via upload

--- Servlet/servlet-first/Person.java
+++ Servlet/servlet-first/Person.java
@@ -1,11 +1,15 @@
-package com.first;
+package com.task1;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class Person {
-	public void eat() {
-		System.out.println("Person is eating");
-		
-	}
+
+    public void eat() {
+        System.out.println("Person is eating");
+    }
+
+    public void sleep() {
+        System.out.println("Person is sleeping");
+    }
 }
